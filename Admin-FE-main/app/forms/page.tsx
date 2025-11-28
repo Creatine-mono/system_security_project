cd8d519ec92ae279c65e5b293363184ee78e54fa
Creatine-mono <psh243360@gmail.com>
2025-11-29 00:39:48 +0900

    ss

--- Admin-FE-main/app/forms/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ContactForm } from "@/components/contact-form"
-
-export default function Page() {
-  return (
-    <div className="flex min-h-svh items-center justify-center">
-      <ContactForm />
-    </div>
-  )
-}
